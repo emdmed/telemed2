3f389b0b923846d21221f3f6f0900f4efae9951c
nevor <nevikhun@gmail.com>
2020-07-22 12:33:52 -0300

    1

--- client/assets/js/payment.js
+++ client/assets/js/payment.js
@@ -6,7 +6,7 @@ setTimeout(() => {
         method: "POST",
         data: {_id: payment.id, patientId: payment.patient.id},
         success: function(res){
-            window.location = "patient.html"
+            window.location = "patient.html" //full url in production
         },
         error: function(){
             alert("Error")
